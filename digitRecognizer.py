dd5001f0faa91742470da7e18c4fc7179054b37d
Alaa ElNouby <alaaelnouby@gmail.com>
2016-02-03 19:19:06 +0200

    fix validation error

--- digitRecognizer.py
+++ digitRecognizer.py
@@ -9,13 +9,13 @@ import matplotlib.cm as cm
 num_lables=10
 image_size=28
 
-learning_rate=1e-4
+learning_rate=1e-2
 batch_size=50
 
 #Read Data
-data=pd.read_csv('train.csv',header=0)
+data=pd.read_csv('../input/train.csv',header=0)
 
-test=pd.read_csv('test.csv',header=0)
+test=pd.read_csv('../input/test.csv',header=0)
 
 
 images=np.array(data.drop('label',axis=1)).astype(np.float32)
@@ -49,42 +49,13 @@ def biases(shape):
     return tf.Variable(tf.constant(0.1,shape=shape))
 
 def conv2d(data,wts):
-    return tf.nn.conv2d(data,wts,strides=[1,1,1,1],padding='SAME',use_cudnn_on_gpu=True)
+    return tf.nn.conv2d(data,wts,strides=[1,1,1,1],padding='SAME',use_cudnn_on_gpu=False)
 
 def max_pooling(value):
     return tf.nn.max_pool(value,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
-def model(data):
-    layer1_w=weights([5,5,1,32])
-    layer1_b=biases([32])
-
-    image=tf.reshape(data,[-1,image_size,image_size,1])
-
-    hidden1_conv=tf.nn.relu(conv2d(image,layer1_w)+layer1_b)
-    hidden1_pool=max_pooling(hidden1_conv)
-
-    #Layer2
-    layer2_w=weights([5,5,32,64])
-    layer2_b=biases([64])
-
-    hidden2_conv=tf.nn.relu(conv2d(hidden1_pool,layer2_w)+layer2_b)
-    hidden2_pool=max_pooling(hidden2_conv)
 
 
-    #fully connected
-    fully_w=weights([7*7*64,1024])
-    fully_b=biases([1024])
-
-    hidden2_pool_flat=tf.reshape(hidden2_pool,[-1,7*7*64])
-    hidden_fully=tf.nn.relu(tf.matmul(hidden2_pool_flat,fully_w)+fully_b)
-
-    #dropout
-    hidden_final=tf.nn.dropout(hidden_fully,keep_prob=1)
-
-    final_w=weights([1024,num_lables])
-    final_b=biases([num_lables])
-
-    return  tf.matmul(hidden_final,final_w)+final_b
 
 
 
@@ -99,6 +70,38 @@ with graph.as_default():
     tf_test_data=tf.constant(test)
     # tf_valid_labels=tf.placeholder(tf.float32,shape=[batch_size,num_lables])
 
+    layer1_w=weights([5,5,1,32])
+    layer1_b=biases([32])
+
+    #Layer2
+    layer2_w=weights([5,5,32,64])
+    layer2_b=biases([64])
+
+    #fully connected
+    fully_w=weights([7*7*64,1024])
+    fully_b=biases([1024])
+    final_w=weights([1024,num_lables])
+    final_b=biases([num_lables])
+
+
+    def model(data):
+
+        image=tf.reshape(data,[-1,image_size,image_size,1])
+
+        hidden1_conv=tf.nn.relu(conv2d(image,layer1_w)+layer1_b)
+        hidden1_pool=max_pooling(hidden1_conv)
+
+        hidden2_conv=tf.nn.relu(conv2d(hidden1_pool,layer2_w)+layer2_b)
+        hidden2_pool=max_pooling(hidden2_conv)
+
+        hidden2_pool_flat=tf.reshape(hidden2_pool,[-1,7*7*64])
+        hidden_fully=tf.nn.relu(tf.matmul(hidden2_pool_flat,fully_w)+fully_b)
+
+        #dropout
+        hidden_final=tf.nn.dropout(hidden_fully,keep_prob=1)
+
+
+        return  tf.matmul(hidden_final,final_w)+final_b
 
 
     logits=model(tf_train_data)
@@ -117,7 +120,7 @@ def accuracy(predictions, labels):
 
 
 
-num_steps=20000
+num_steps=3001
 
 with tf.Session(graph=graph) as session:
 
@@ -140,7 +143,7 @@ with tf.Session(graph=graph) as session:
 
         _,l, predictions = session.run([optimization, loss, train_predections], feed_dict=feed_dict)
 
-        if(step%200==0):
+        if(step%500==0):
             print('Minibatch loss at step %d: %f' % (step, l))
             print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
             print('Validation accuracy: %.1f%%' % accuracy(valid_predections.eval(), validation_labels))
